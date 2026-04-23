0ab24add46000890bb8113b8622550c4a732b48d
Maksym <maksym.mazurenko@gmail.com>
2026-04-23 21:47:37 +0300

    correct solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     storage = {}
-    def wrapper(*args):
+
+    def wrapper(*args) -> Any:
         if args in storage:
             print("Getting from cache")
             return storage[args]
@@ -14,13 +15,16 @@ def cache(func: Callable) -> Callable:
             return result
     return wrapper
 
+
 @cache
 def long_time_func(a: int, b: int, c: int) -> int:
     return (a ** b ** c) % (a * c)
 
+
 @cache
 def long_time_func_2(n_tuple: tuple, power: int) -> int:
     return [number ** power for number in n_tuple]
+
 
 long_time_func(1, 2, 3)
 long_time_func(2, 2, 3)
@@ -28,10 +32,3 @@ long_time_func_2((5, 6, 7), 5)
 long_time_func(1, 2, 3)
 long_time_func_2((5, 6, 7), 10)
 long_time_func_2((5, 6, 7), 10)
-
-# Calculating new result
-# Calculating new result
-# Calculating new result
-# Getting from cache
-# Calculating new result
-# Getting from cache
